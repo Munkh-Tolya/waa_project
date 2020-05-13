789b8bfb6e733a2b228c390685f445e45089630c
oogii0713 <oogii0713@users.noreply.github.com>
2020-05-12 21:59:13 -0500

    Global interceptor config updated

--- src/main/java/edu/miu/cs545/waa_project/config/WebConfig.java
+++ src/main/java/edu/miu/cs545/waa_project/config/WebConfig.java
@@ -1,11 +1,13 @@
 package edu.miu.cs545.waa_project.config;
 
+import edu.miu.cs545.waa_project.Interceptor.Interceptor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -29,5 +31,15 @@ public class WebConfig implements WebMvcConfigurer {
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(messageSource());
         return bean;
+    }
+
+    @Bean
+    public Interceptor productServiceInterceptor() {
+        return new Interceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(productServiceInterceptor());
     }
 }
