4018b290f122d441bd93a36718e9d2e5bcef0e39
Odko <n.odbayar@gmail.com>
2020-05-13 13:49:17 -0500

    Buyer Seller registration done

--- src/main/java/edu/miu/cs545/waa_project/controller/AccountController.java
+++ src/main/java/edu/miu/cs545/waa_project/controller/AccountController.java
@@ -1,12 +1,23 @@
 package edu.miu.cs545.waa_project.controller;
 
+import edu.miu.cs545.waa_project.domain.Buyer;
+import edu.miu.cs545.waa_project.domain.Seller;
+import edu.miu.cs545.waa_project.domain.User;
+import edu.miu.cs545.waa_project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/login")
     public String login() {
@@ -21,17 +32,39 @@ public class AccountController {
     }
 
     @GetMapping("/sign-up")
-    public String signUp() {
+    public String signUp(@ModelAttribute("user") User user) {
         return "account/sign-up";
     }
 
-    @GetMapping("/sign-up/partner")
-    public String signUpSeller() {
-        return "account/sellerRegister";
-    }
+    @PostMapping("/sign-up")
+    public String createNewUser(@Valid User user, BindingResult bindingResult,Model model, @RequestParam String role) {
+        User userExists = userService.findByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult.rejectValue("email", "error.user","Email address has been registered already");
+        }
 
-    @GetMapping("/sign-up/customer")
-    public String signUpCustomer() {
-        return "account/buyerRegister";
+        if (bindingResult.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        System.out.println(role);
+        if (role.equals("Buyer")) {
+            Buyer buyer = new Buyer();
+            buyer.setFirstName(user.getFirstName());
+            buyer.setLastName(user.getLastName());
+            buyer.setPassword(user.getPassword());
+            buyer.setEmail(user.getEmail());
+            userService.save(buyer);
+        } else {
+            Seller seller = new Seller();
+            seller.setFirstName(user.getFirstName());
+            seller.setLastName(user.getLastName());
+            seller.setPassword(user.getPassword());
+            seller.setEmail(user.getEmail());
+            userService.save(seller);
+        }
+
+        model.addAttribute("successMessage", "User has been registered successfully");
+        return "account/sign-up";
     }
 }
