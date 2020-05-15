b56937b19a4344d6c99ea7348550d4c29e907538
Munkh-Tolya <60638000+Munkh-Tolya@users.noreply.github.com>
2020-05-15 16:13:40 -0500

    Fixed bug in remove card section

--- src/main/java/edu/miu/cs545/waa_project/service/ItemServiceImpl.java
+++ src/main/java/edu/miu/cs545/waa_project/service/ItemServiceImpl.java
@@ -3,6 +3,7 @@ package edu.miu.cs545.waa_project.service;
 import edu.miu.cs545.waa_project.domain.Item;
 import edu.miu.cs545.waa_project.domain.Product;
 import edu.miu.cs545.waa_project.repository.ItemRepository;
+import edu.miu.cs545.waa_project.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 public class ItemServiceImpl implements ItemService{
     @Autowired
     private ItemRepository itemRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public Item findTopByProduct(Product product) {
@@ -17,7 +20,11 @@ public class ItemServiceImpl implements ItemService{
     }
 
     @Override
-    public void deleteItemById(Long productId) {
-        itemRepository.deleteById(productId);
+    public void deleteItemById(Long itemId) {
+        Item item = itemRepository.findById(itemId).get();
+        Product product = item.getProduct();
+        product.setQuantity(product.getQuantity() + item.getQuantity());
+        productRepository.save(product);
+        itemRepository.deleteById(itemId);
     }
 }
